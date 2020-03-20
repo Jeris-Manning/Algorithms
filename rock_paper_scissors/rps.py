c6a332a0a8da66c38ea3281b8f6c606cdd5a37ad
Jeris Manning <the.goose.moose@gmail.com>
2020-03-19 18:33:09 -0600

    adds rps solution

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,24 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+    classic = [["rock"], ["paper"], ["scissors"]]
+    if n == 0:
+        return [[]]
+    if n == 1:
+        return [["rock"], ["paper"], ["scissors"]]
+
+    move_list = []
+
+    for tools in (rock_paper_scissors(n-1)):
+       move_list += [tools + classic[0]]
+       move_list += [tools + classic[1]]
+       move_list += [tools + classic[2]]
+
+    return move_list
+
+print(rock_paper_scissors(4))
+
+
 
 
 if __name__ == "__main__":
@@ -12,3 +29,6 @@ if __name__ == "__main__":
     print(rock_paper_scissors(num_plays))
   else:
     print('Usage: rps.py [num_plays]')
+
+
+
